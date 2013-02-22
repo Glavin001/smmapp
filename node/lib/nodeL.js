a88cc4130d450f2d7903d34344c3ad62034e6928
= <=>
2013-02-21 22:50:59 -0400

    Updating for merge.

---
+++ node/lib/nodeL.js
@@ -0,0 +1,187 @@
+/* 
+ * Author : Dawson
+ * Date : Feb 18, 2013
+ * File : nodeL.js
+ */
+
+//var MNLU_fs = module.require('fs'); // file reading and writing
+
+/*
+ * 
+ * @type Enum
+ */
+exports.LOG_LEVEL = {};
+exports.LOG_LEVEL.Id = 'LOG_LEVEL';
+exports.LOG_LEVEL = {
+    Id : exports.LOG_LEVEL.Id,
+           
+   LOW : {
+       Id : exports.LOG_LEVEL.Id, 
+       TYPE : 'LOW'
+   },
+   MED : {
+       Id : exports.LOG_LEVEL.Id,
+       TYPE : 'MEDIUM'
+   },
+   HIGH : {
+       Id : exports.LOG_LEVEL.Id,
+        TYPE : 'HIGH'
+   }
+};
+
+/*
+ * 
+ * @type Enum
+ */
+exports.LOG_MEDIUM = {};
+exports.LOG_MEDIUM.Id = 'LOG_MEDIUM';
+exports.LOG_MEDIUM = {
+    Id : exports.LOG_MEDIUM.Id,
+            
+    CONSOLE : {
+        Id : exports.LOG_MEDIUM.Id,
+        TYPE : 'CONSOLE'
+    },
+    FILE : {
+        Id : exports.LOG_MEDIUM.Id,
+        TYPE : 'FILE'
+    },
+    MYSQL : {
+        Id : exports.LOG_MEDIUM.Id,
+        TYPE : 'MYSQL'
+    }
+};
+
+/*
+ * 
+ * @type Enum
+ */
+exports.LOG_TYPE = {};
+exports.LOG_TYPE.Id = 'LOG_TYPE';
+exports.LOG_TYPE = {
+    Id : exports.LOG_TYPE.Id,
+    
+    INFO : {
+        Id : exports.LOG_TYPE.Id, 
+        TYPE : 'INFO'
+    },
+    WARNING : {
+        Id : exports.LOG_TYPE.Id,
+        TYPE : 'WARNING'
+    },
+    EXCEPTION : {
+        Id : exports.LOG_TYPE.Id,
+        TYPE : 'EXCEPTION'
+    },
+    ERROR : {
+        Id : exports.LOG_TYPE.Id,
+        TYPE : 'ERROR'
+    }
+};
+
+/*
+ * arguments : 
+ * 0 - logging medium
+ * 1 - default logging type
+ * 2 - default logging level
+ * 
+ * @type Object
+ */
+exports.Logger = function() {
+
+    var args = arguments;
+    if (args.length > 0) {
+        if (args[0].Id === exports.LOG_MEDIUM.Id)
+        {
+            this.logMedium = args[0];
+        } else {
+            throw new 'Invalid arg : 1st arg must be of type LOG_MEDIUM';
+        }
+    } else {
+        this.logMedium = exports.LOG_MEDIUM.CONSOLE;
+    }
+    
+    if (args.length > 1) {
+        if (args[1].Id === exports.LOG_TYPE.Id) {
+            this.defaultLogType = args[1];
+        } else {
+            throw 'Invalid arg : 2nd arg must be of type LOG_TYPE';
+        }
+    } else {
+        this.defaultLogType = exports.LOG_TYPE.INFO;
+    }
+    
+    if (args.length > 2) {
+        if (args[2].Id === exports.LOG_LEVEL.Id) {
+            this.defaultLogLevel = args[2];
+        } else {
+            throw 'Invalid arg : 3rd arg must be of type LOG_LEVEL';
+        }
+    } else {
+        this.defaultLogLevel = exports.LOG_LEVEL.LOW;
+    }
+    
+    if (args.length > 3) {
+        throw 'Invalid number of arguments : ' + args.length;
+    }
+    
+    // private methods
+
+    var formatLogMessage = function(logMessage, logLevel, logType) {
+        
+        return ('[ ' + logType.TYPE + ' | ' + logLevel.TYPE + ' ] : ' + logMessage);
+    };
+    
+    var writeLog = function(logMessage, logMedium) {
+        
+        if (arguments.length !== 2) {
+            throw '[writeLog] Invalid number of arguments : ' + args.length;
+        }
+        
+        if (logMedium.Id === exports.LOG_MEDIUM.Id) {
+            if (logMedium.TYPE === exports.LOG_MEDIUM.CONSOLE.TYPE)
+            {
+                console.log(logMessage);
+            } else {
+                throw 'Not yet implemented log medium.';
+            }
+        } else {
+            throw '[writeLog] Invalid arg : 2nd arg must be of tye LOG MEDIUM.';
+        }
+    };
+    
+    this.log = function(logMessage) {
+       
+        var
+            args = arguments,
+            logLevel = this.defaultLogLevel,
+            logType = this.defaultLogType;
+    
+        if (args.length === 2) {
+            if (args[1].Id === exports.LOG_LEVEL.Id) {
+                logLevel = args[1];
+            } else if (args[1].Id === exports.LOG_TYPE.Id) {
+                logType = args[1];
+            } else {
+                throw 'Invalid arg : 2nd of 2 args must be either of type LOG_LEVEL, or LOG_TYPE';
+            }
+        } else if (args.length === 3) {
+            if (args[1].Id === exports.LOG_LEVEL.Id &&
+                args[2].Id === exports.LOG_TYPE.Id) {
+                logLevel = args[1];
+                logType = args[2];
+            } else {
+                throw 'Invalid arg : 2nd and 3rd arg must be of type LOG_LEVEL and LOG_TYPE.';
+            }
+        } else if (args.length > 3) {
+            throw 'Invalid number of arguments : ' + args.length;
+        }
+        
+        logMessage = formatLogMessage(logMessage, logLevel, logType);
+        writeLog(logMessage, this.logMedium);
+    };
+};
+
+
+
+
--- node/server.js
+++ node/server.js
@@ -3,20 +3,75 @@
  * Feb 19, 2013
  */
 
+// constants
+var 
+    LOGGING = true,
+    DEBUGGING = true;
+    
+/*
+* setup the logger if logging enabled
+*/
+var logger = undefined;
+try {
+    
+    var nodeL = require('./lib/nodeL');
+    logger = new nodeL.Logger
+            (
+            nodeL.LOG_MEDIUM.CONSOLE,
+            nodeL.LOG_TYPE.INFO,
+            nodeL.LOG_LEVEL.LOW
+            );            
+    logger.log('Logging ...');
+    
+} catch (e) {
+    console.log("ERROR : Could not setup logger.");
+    console.log("\tREASON : " + e)
+    process.exit(1);
+}
+
+logger.log('Loading modules.');
+
 var 
     http = require('http'),
     path = require('path'),
     fs = require('fs');
 
+logger.log('Loading functions.');
+
+function requestEnded(error) {
+    logger.log('\tRequest ended.');
+}
+
+function requestClosed(error) {
+    logger.log('\tRequest closed.');
+}
+
+function attainContentType(url) {
+    
+    var 
+        extname = path.extname(url),
+        contentType = 'text/html';
+    
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+    return contentType;
+}
+
 function handleRequest(request, response) {
    
-    console.log('request starting...');
+    logger.log('\tRequest started.');
      
-    var filePath = '.' + request.url;
-    if (filePath === './')
-        filePath = './index.htm';
+    // register request events
+    request.on('end', requestEnded);
+    request.on('close', requestClosed);
      
-    path.exists(filePath, function(exists) {
+    fs.exists('../app.html', function(exists) {
 
             if (exists) {
                 fs.readFile('../app.html', function(error, content) {
@@ -36,6 +91,9 @@ function handleRequest(request, response) {
             }
     });
 };
+
+console.log(__dirname);
+logger.log('Starting server.');
 
 // create and init my server
 var server = http.createServer();
